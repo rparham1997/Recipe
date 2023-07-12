edd80d2744c20df51a902639bfca8839c6fa2969
rparham1997 <rparham1997@gmail.com>
2023-07-11 22:03:15 -0400

    first commit

---
+++ Recipe/Models/RecipeCategoryResponse.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeCategoryResponse.swift
+//  Recipe
+//
+//  Created by Ramar Parham on 7/11/23.
+//
+
+import Foundation
---
+++ Recipe/Services/Webservice.swift
@@ -0,0 +1,8 @@
+//
+//  Webservice.swift
+//  Recipe
+//
+//  Created by Ramar Parham on 7/11/23.
+//
+
+import Foundation

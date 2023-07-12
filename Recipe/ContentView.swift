2772b22ead7f418efb1375c5d8b5839323486763
rparham1997 <rparham1997@gmail.com>
2023-07-11 21:48:52 -0400

    Initial Commit

---
+++ Recipe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Recipe
+//
+//  Created by Ramar Parham on 7/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Ramar Parham on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

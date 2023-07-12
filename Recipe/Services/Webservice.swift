b2688dc7a341741d12b2a68a55750b7fa0ceddfc
rparham1997 <rparham1997@gmail.com>
2023-07-11 22:04:19 -0400

    Quiz Game

--- Recipe/Services/Webservice.swift
+++ Recipe/Services/Webservice.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case badRequest
+    case decodingError
+}
+
+
+class Webservice {
+    
+    func get<T: Decodable>(url: URL, parse: (Data) -> T?) async throws -> T {
+        
+      let (data, response) = try await URLSession.shared.data(from: url)
+        
+        if (response as? HTTPURLResponse)?.statusCode != 200 {
+            throw NetworkError.badRequest
+        }
+        
+        guard let result = parse(data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return result
+    }
+}
